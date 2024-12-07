365b4ecfaebc58640e91734aad551d5c62204570
Sadra Mousavi <saysadra@gmail.com>
2024-12-08 01:11:31 +0330

    Add project files.

---
+++ Core/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string ISBN { get; set; }
+
+        public Product(string name, int price, string isbn)
+        {
+            Name = name;
+            Price = price;
+            ISBN = isbn;
+        }
+
+        public static string inputString;
+        public static int inputNumber;
+        public static bool isValidInput;
+
+    }
+}
---
+++ Presentation/Program.cs
@@ -0,0 +1,31 @@
+﻿using Core.Models;
+using DataAccess.ListAndMethods;
+
+namespace Presentation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Product.inputString = Console.ReadLine();
+                Product.isValidInput = int.TryParse(Product.inputString, out Product.inputNumber);
+                if (Product.isValidInput)
+                {
+                    switch (Product.inputNumber)
+                    {
+                        case 0:
+                            {
+
+                            }return;
+                        default:
+                            {
+                                Console.WriteLine("enter valid option!");
+                            }break;
+                    }
+                }
+            }
+        }
+    }
+}
