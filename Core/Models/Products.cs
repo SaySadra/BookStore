9d97932f03a233629df5a5a4d7b4d4936adffc9d
Sadra Mousavi <saysadra@gmail.com>
2024-12-09 21:19:25 +0330

    fix property bugs

---
+++ Core/Models/Products.cs
@@ -0,0 +1,48 @@
+﻿namespace Core.Models
+{
+    public class Products
+    {
+        private string _name;
+        private double _price;
+        private string _isbn;
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }
+            set
+            {
+                _price = value;
+            }
+        }
+        public string ISBN
+        {
+            get
+            {
+                return _isbn;
+            }
+            set
+            {
+                _isbn = value;
+            }
+        }
+        public Products(string name, double price, string isbn)
+        {
+            Name = name;
+            Price = price;
+            ISBN = isbn;
+        }
+    }
+}
